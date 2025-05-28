8e5e34df7bfbd094207cae453da110a395ff3380
Dylan Cobb <dylancobb92@gmail.com>
2025-05-28 16:36:02 +0100

    make title fully responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,20 @@
 </script>
 
 <div class="mt-24 flex flex-col items-center gap-4 text-center">
-	<h1 class="text-3xl lg:text-4xl xl:text-5xl">31-Tone Pitch Class Sets</h1>
-	<div class="text-lg lg:text-xl xl:text-2xl">
+	<h1 class="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl">31-Tone Pitch Class Sets</h1>
+	<div class="text-base sm:text-lg lg:text-xl xl:text-2xl">
 		a simple system for representing, querying and manipulating
 	</div>
-	<div class="flex justify-center gap-4 text-sm lg:text-base xl:text-lg">
-		<div class="bg-green-200 px-4 py-2 shadow-[5px_5px_0_0_#44403b]">notes</div>
-		<div class="bg-red-200 px-4 py-2 shadow-[5px_5px_0_0_#44403b]">intervals</div>
-		<div class="bg-yellow-200 px-4 py-2 shadow-[5px_5px_0_0_#44403b]">keys</div>
-		<div class="bg-blue-200 px-4 py-2 shadow-[5px_5px_0_0_#44403b]">modes</div>
-		<div class="bg-violet-200 px-4 py-2 shadow-[5px_5px_0_0_#44403b]">enharmonics</div>
+	<div class="flex flex-wrap justify-center gap-4 text-sm lg:text-base xl:text-lg">
+		<div class="bg-green-200 px-3 py-1.5 shadow-[5px_5px_0_0_#44403b] md:px-4 md:py-2">notes</div>
+		<div class="bg-red-200 px-3 py-1.5 shadow-[5px_5px_0_0_#44403b] md:px-4 md:py-2">intervals</div>
+		<div class="bg-yellow-200 px-3 py-1.5 shadow-[5px_5px_0_0_#44403b] md:px-4 md:py-2">keys</div>
+		<div class="bg-blue-200 px-3 py-1.5 shadow-[5px_5px_0_0_#44403b] md:px-4 md:py-2">modes</div>
+		<div class="bg-violet-200 px-3 py-1.5 shadow-[5px_5px_0_0_#44403b] md:px-4 md:py-2">
+			enharmonics
+		</div>
 	</div>
-	<div class="text-center text-lg lg:text-xl xl:text-2xl">
+	<div class="text-center text-base sm:text-lg lg:text-xl xl:text-2xl">
 		while preserving semantic meaning in tonal music.
 	</div>
 </div>
