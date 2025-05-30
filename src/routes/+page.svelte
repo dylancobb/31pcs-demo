233ff6168d33ec7b2556c2bb0cdda9db763c63cc
Dylan Cobb <dylancobb92@gmail.com>
2025-05-30 20:51:51 +0100

    update scales explanation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,26 +54,31 @@
 		class="flex w-full flex-col gap-4 border-4 border-stone-400 bg-stone-600 p-8 text-white shadow-[7px_7px_0_0_var(--color-stone-500)]"
 	>
 		<h2 class="text-lg sm:text-xl lg:text-2xl xl:text-3xl">Keys / modes</h2>
-		<ul class="list-inside list-[square] text-sm lg:text-base xl:text-lg">
+		<ul class="list-[square] pl-4 text-sm lg:text-base xl:text-lg">
 			<li>
 				We define a key or mode as a sequence of 17 perfect fifths:
-				<ul class="list-inside list-[square] pl-4 text-sm lg:text-base xl:text-lg">
+				<ul class="list-[square] pl-4">
 					<li>7 diatonic scale degrees.</li>
 					<li>5 chromatically raised degrees.</li>
 					<li>5 chromatically lowered degrees.</li>
 				</ul>
 			</li>
 			<li>
-				This is because <span
+				We stop at 17 because <span
 					class="underline decoration-stone-200 decoration-dotted underline-offset-4 sm:underline-offset-5 xl:underline-offset-6"
 					>well-behaved</span
 				>
-				chromatically altered notes resolve by
+				chromatic notes resolve by
 				<span
 					class="underline decoration-stone-200 decoration-dotted underline-offset-4 sm:underline-offset-5 xl:underline-offset-6"
 					>diatonic half-step</span
-				> in the direction of their alteration. Any more notes would be enharmonic to diatonic scale
-				degrees, and have nowhere available to correctly resolve to.
+				> in the direction of their alteration (e.g. C# -&gt; D in C major). Further extensions would
+				have nowhere to correctly resolve to, and produce enharmonics with unaltered scale degrees.
+			</li>
+			<li>
+				Beginning with C major, we can continue by fifth to define seven more keys in either
+				direction, up to C# and down to Cb major (likewise for their associated modes). Doing so
+				precisely exhausts the notes defined by the 31-tone system.
 			</li>
 		</ul>
 	</section>
